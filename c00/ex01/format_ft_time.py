9797dfde8944467c247c5f36a5e1690947079be9
WilliamsOz <williams.ozserttas05@gmail.com>
2024-10-22 15:59:33 +0200

    End of ex01

--- c00/ex01/format_ft_time.py
+++ c00/ex01/format_ft_time.py
@@ -1,1 +1,14 @@
-fr
+from time import time
+from datetime import datetime
+
+
+timestamp = time()
+
+print("Seconds since January 1, 1970:", "{:,.4f}".format(timestamp), "or",
+      "{:.3e}".format(timestamp), "in scientific notation")
+
+date = datetime.now()
+
+date = date.strftime("%b %d %Y")
+
+print(date)
