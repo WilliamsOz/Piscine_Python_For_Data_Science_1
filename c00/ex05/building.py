c731f80cbae8bfccbe53f470744a299bfada9395
WilliamsOz <williams.ozserttas05@gmail.com>
2024-10-23 10:15:04 +0200

    End of ex05

--- c00/ex05/building.py
+++ c00/ex05/building.py
@@ -3,46 +3,101 @@ from string import punctuation
 
 
 def catch_input():
-    str = input("What is the text to count?\n")
-    return str
+    """"
+    Ask for input in case no string is given.
+
+    Return the input.
+    """
+    try:
+        string = input("What is the text to count?\n")
+    except EOFError:
+        pass
+    return str(string)
 
 
 def n_lower_chars(string):
+    """"
+    Return number of lower char in string.
+    """
     return sum(1 for c in string if c.islower())
 
 
 def n_upper_chars(string):
+    """"
+    Return number of upper char in string.
+    """
     return sum(1 for c in string if c.isupper())
 
 
 def n_punc_chars(string):
-    print("CCCCC", str(string).count(punctuation))
-    return 1
+    """"
+    Return number of punctuation in string.
+    """
+    count = 0
+    for i in range(0, len(string)):
+        if string[i] in punctuation:
+            count += 1
+    return count
 
 
-def get_data(str):
+def n_space_chars(string):
+    """"
+    Return number of spaces in string.
+    """
+    return sum(1 for c in string if c == ' ')
+
+
+def n_digit_chars(string):
+    """"
+    Return number of digits in string.
+    """
+    return sum(1 for c in string if c.isdigit())
+
+
+def get_data(string):
+    """"
+    Get composition data of string.
+
+    Return data as a dictionary.
+    """
     data = {}
-    data["len"] = len(str)
-    data["upper_chars"] = n_upper_chars(str)
-    data["lower_chars"] = n_lower_chars(str)
-    data["punct_chars"] = n_punc_chars(str)
+    data["len"] = len(string)
+    data["upper_chars"] = n_upper_chars(string)
+    data["lower_chars"] = n_lower_chars(string)
+    data["punct_chars"] = n_punc_chars(string)
+    data["space_chars"] = n_space_chars(string)
+    data["digit_chars"] = n_digit_chars(string)
     return data
 
 
+def print_data(data):
+    """
+    Print the composition of the string.
+    """
+    print("The text contains", data["len"], "characters:")
+    print(data["upper_chars"], "upper letters")
+    print(data["lower_chars"], "lower letters")
+    print(data["punct_chars"], "punctuation marks")
+    print(data["space_chars"], "spaces")
+    print(data["digit_chars"], "digits")
+
+
 def main():
+    """"
+    Analyzes the given text and print its char composition.
+
+    Ask for input if no input is given.
+    """
     try:
         data = {}
         if len(sys.argv) > 2:
             raise AssertionError("more than one argument is provided")
         elif len(sys.argv) == 1:
-            str = catch_input()
-            data = get_data(str)
+            string = catch_input()
+            data = get_data(string)
         else:
             data = get_data(sys.argv[1])
-        print(data["len"])
-        print(data["upper_chars"])
-        print(data["lower_chars"])
-        print(data["punct_chars"])
+        print_data(data)
     except AssertionError as error:
         print(AssertionError.__name__ + ":", error)
         exit(1)
